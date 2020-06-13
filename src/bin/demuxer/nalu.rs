92a7ffcf9048c15a4a68668196897b06866dc7e3
Rain Liu <yuliu@outlook.com>
2020-06-13 15:16:08 -0700

    fix nal_unit_length with LittleEndian

--- src/bin/demuxer/nalu.rs
+++ src/bin/demuxer/nalu.rs
@@ -1,6 +1,6 @@
 use super::Demuxer;
 
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
 
 use std::fs::File;
 use std::io;
@@ -26,7 +26,7 @@ impl NaluDemuxer {
 impl Demuxer for NaluDemuxer {
     fn read(&mut self) -> io::Result<Packet> {
         //TODO: To be confirmed with ETM, is it ok from endianness perspective?
-        let nal_unit_length = self.reader.read_u32::<BigEndian>()?;
+        let nal_unit_length = self.reader.read_u32::<LittleEndian>()?;
         let mut data: Vec<u8> = vec![0; nal_unit_length as usize];
         self.reader.read_exact(&mut data)?;
 
