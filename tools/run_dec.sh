1acdc59465c1760134b2486de9dbfd05da318e45
Rain Liu <yuliu@outlook.com>
2020-07-20 01:01:21 -0700

    update tools script

--- tools/run_dec.sh
+++ tools/run_dec.sh
@@ -1,42 +1,24 @@
 #!/bin/sh
 
-#./run_dec.sh 1>tmp/result.txt 2>&1
-
 for QP in 22 27 32 37
 do
+../target/debug/revcd.exe -i ./data/test_ld_p_nodb_q${QP}.evc   -o ./tmp/test_ld_p_nodb_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ld_p_nodb_q${QP}.txt
 
-./evca_decoder.exe        -i ./data/test_ld_p_nodb_q${QP}.evc                     -o ./tmp/test_ld_p_nodb_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ld_p_nodb_q${QP}.evc                     -o ./tmp/test_ld_p_nodb_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ld_p_nodb_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ld_p_nodb_q'${QP}'_revc.yuv"}' > ./tmp/test_ld_p_nodb_q${QP}.txt
-md5sum -c                     ./tmp/test_ld_p_nodb_q${QP}.txt
+../target/debug/revcd.exe -i ./data/test_ld_b_nodb_q${QP}.evc   -o ./tmp/test_ld_b_nodb_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ld_b_nodb_q${QP}.txt
 
-./evca_decoder.exe        -i ./data/test_ld_b_nodb_q${QP}.evc                     -o ./tmp/test_ld_b_nodb_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ld_b_nodb_q${QP}.evc                     -o ./tmp/test_ld_b_nodb_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ld_b_nodb_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ld_b_nodb_q'${QP}'_revc.yuv"}' > ./tmp/test_ld_b_nodb_q${QP}.txt
-md5sum -c                     ./tmp/test_ld_b_nodb_q${QP}.txt
+../target/debug/revcd.exe -i ./data/test_ra_b_nodb_q${QP}.evc   -o ./tmp/test_ra_b_nodb_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ra_b_nodb_q${QP}.txt
 
-./evca_decoder.exe        -i ./data/test_ra_b_nodb_q${QP}.evc                     -o ./tmp/test_ra_b_nodb_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ra_b_nodb_q${QP}.evc                     -o ./tmp/test_ra_b_nodb_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ra_b_nodb_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ra_b_nodb_q'${QP}'_revc.yuv"}' > ./tmp/test_ra_b_nodb_q${QP}.txt
-md5sum -c                     ./tmp/test_ra_b_nodb_q${QP}.txt
+../target/debug/revcd.exe -i ./data/test_ld_p_q${QP}.evc        -o ./tmp/test_ld_p_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ld_p_q${QP}.txt
 
-##########################################################################################################################
+../target/debug/revcd.exe -i ./data/test_ld_b_q${QP}.evc        -o ./tmp/test_ld_b_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ld_b_q${QP}.txt
 
-./evca_decoder.exe        -i ./data/test_ld_p_q${QP}.evc                     -o ./tmp/test_ld_p_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ld_p_q${QP}.evc                     -o ./tmp/test_ld_p_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ld_p_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ld_p_q'${QP}'_revc.yuv"}' > ./tmp/test_ld_p_q${QP}.txt
-md5sum -c                     ./tmp/test_ld_p_q${QP}.txt
-
-./evca_decoder.exe        -i ./data/test_ld_b_q${QP}.evc                     -o ./tmp/test_ld_b_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ld_b_q${QP}.evc                     -o ./tmp/test_ld_b_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ld_b_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ld_b_q'${QP}'_revc.yuv"}' > ./tmp/test_ld_b_q${QP}.txt
-md5sum -c                     ./tmp/test_ld_b_q${QP}.txt
-
-./evca_decoder.exe        -i ./data/test_ra_b_q${QP}.evc                     -o ./tmp/test_ra_b_q${QP}_evca.yuv -v 1
-../target/debug/revcd.exe -i ./data/test_ra_b_q${QP}.evc                     -o ./tmp/test_ra_b_q${QP}_revc.yuv -v
-md5sum -b                     ./tmp/test_ra_b_q${QP}_evca.yuv | awk '{print $1,"./tmp/test_ra_b_q'${QP}'_revc.yuv"}' > ./tmp/test_ra_b_q${QP}.txt
-md5sum -c                     ./tmp/test_ra_b_q${QP}.txt
-
+../target/debug/revcd.exe -i ./data/test_ra_b_q${QP}.evc        -o ./tmp/test_ra_b_q${QP}_revc.yuv
+md5sum -c                    ./data/test_ra_b_q${QP}.txt
 done
 
 
